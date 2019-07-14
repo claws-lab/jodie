b83c8157004113f0c64d80d8901378ccc548d1ba
srijankr <srijan@stanford.edu>
2019-07-14 16:41:35 -0700

    code to download datasets to the data/ directory

---
+++ download_data.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+wget http://snap.stanford.edu/jodie/reddit.csv data/
+wget http://snap.stanford.edu/jodie/wikipedia.csv data/
+wget http://snap.stanford.edu/jodie/mooc.csv data/
+wget http://snap.stanford.edu/jodie/lastfm.csv data/
